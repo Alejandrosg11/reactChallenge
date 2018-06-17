b3d34f96f57fdd1b8be0863be58a67c70f525416
Alex <alejandrosg11@gmail.com>
2018-06-17 17:33:58 -0500

    pequeño cambio al grid

--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,53 +1,65 @@
 import React from "react";
-import { Jumbotron, Button } from 'reactstrap';
-import './grid.css'
-
+import { Jumbotron, Button } from "reactstrap";
+import "./grid.css";
 
 class Grid extends React.Component {
-    render() {
-        let level = 1;
-        let score = 0;
-        const fail = <Button color="danger" onClick={()=>{console.log("Perdistes")}}></Button>;
-        const success = <Button color="success" onClick={()=>{console.log("Ganastes"); level++; score++; console.log(score)}}></Button>;
+  constructor(props) {
+    super(props);
+    this.state = {
+      score: 0,
+      level: 1
+    };
+  }
 
-        const grid = [
-            fail,
-            fail,
-            success,
-            fail,
-        ]
+  render() {
+    const fail = (
+      <Button
+        color="danger"
+        onClick={() => {
+          console.log("Perdiste");
+        }}
+      />
+    );
+    const success = (
+      <Button
+        color="success"
+        onClick={() => {
+          console.log("Ganaste");
+          this.state.score++;
+          console.log("Score: " + this.seState.score);
+          grid.push(fail);
+        }}
+      />
+    );
 
-        let shuffle = (a) => {
-            for (let i = a.length - 1; i > 0; i--) {
-                const j = Math.floor(Math.random() * (i + 1));
-                [a[i], a[j]] = [a[j], a[i]];
-            }
-            return a;
-        }
+    const grid = [fail, fail, success, fail];
 
-        shuffle(grid)
+    let shuffle = a => {
+      for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+      }
+      return a;
+    };
 
+    shuffle(grid);
 
-        return (
-
-            <div>
-            <Jumbotron>
-                <h1 className="display-3">React Challenge</h1>
-                <p className="lead">Click on the Green Button in order to get to the next level</p>
-                <hr className="my-2" />
-                <div className="game">
-                    {grid}
-                </div>
-                <div>
-                    <p>Level: {level}</p>
-                    <p>Score: {score}</p>
-                </div>
-            </Jumbotron>
+    return (
+      <div>
+        <Jumbotron>
+          <h1 className="display-3">React Challenge</h1>
+          <p className="lead">
+            Click on the Green Button in order to get to the next level
+          </p>
+          <hr className="my-2" />
+          <div className="game">{grid}</div>
+          <div>
+            <p>Score: {this.state.score}</p>
           </div>
-            
-        );
-    }
+        </Jumbotron>
+      </div>
+    );
+  }
 }
-    
 
-  export default Grid;
+export default Grid;
